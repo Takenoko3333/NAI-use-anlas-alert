39ffac321499924a3b4bd8d38c41149efd6642bc
Takenoko3333 <153407565+Takenoko3333@users.noreply.github.com>
2023-12-27 19:37:04 +0900

    Update NAI-use-anlas-alert.user.js

--- NAI-use-anlas-alert.user.js
+++ NAI-use-anlas-alert.user.js
@@ -5,7 +5,7 @@
 // @namespace    https://github.com/Takenoko3333/NAI-use-anlas-alert
 // @version      1.0.1
 // @description  On NovelAI's image generation screen, change the color of the generate button to a warning color when there are points to be used.
-// @description:ja  NovelAIの画像生成画面において、使用するポイントがある場合に生成ボタンの色を警告色に変更します
+// @description:ja  NovelAIの画像生成画面において、使用するポイントがある場合に生成ボタンの色を警告色に変更します。
 // @description:en  On NovelAI's image generation screen, change the color of the generate button to a warning color when there are points to be used.
 // @author       Takenoko3333
 // @match        https://novelai.net/image
